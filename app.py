0eeec11c5d9ed927bce1054cf3bd79d7fa3a37d3
manideep <jakkamanideep@gmail.com>
2026-05-16 11:28:57 -0700

    Added Flask COINVERTER app files

--- app.py
+++ app.py
@@ -12,9 +12,9 @@ def home():
         from_currency = request.form["from"]
         to_currency = request.form["to"]
         result = c.convert(amount, from_currency, to_currency)
-        return redirect(url_for('home',results=result))
+        return redirect(url_for('home',result=result))
     result=request.args.get("result")
-    return render_template('index.html',results=result)
+    return render_template('index.html',result=result)
     
 
 if __name__ == "__main__":
